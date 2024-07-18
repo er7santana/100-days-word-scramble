49b49b05b82ee0acc81d6837738aa02b0afe93f0
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-07-18 08:37:39 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
